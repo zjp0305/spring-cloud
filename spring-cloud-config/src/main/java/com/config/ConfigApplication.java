90164d52b848e029023d9670f583b8c6cb580a46
zhangjp0305@gmail.com <zjp19860305>
2018-07-10 22:15:57 +0800

    add config server

--- spring-cloud-config/src/main/java/com/config/ConfigApplication.java
+++ spring-cloud-config/src/main/java/com/config/ConfigApplication.java
@@ -2,9 +2,13 @@ package com.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
 
